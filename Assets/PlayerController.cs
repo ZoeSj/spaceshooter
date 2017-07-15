d93b25987f3b72e6e2221aebc27f477b59b0e46d
e <shengjiamo@163.com>
2017-07-15 11:29:37 +0800

    space

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate(){
+		//获取水平轴的值
+		float h = Input.GetAxis("Horizontal");
+		//获取垂直轴的值
+		float v = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3 (h, 0f, v);
+		rigidbody.velocity = movement * 10;
+	}
+}
